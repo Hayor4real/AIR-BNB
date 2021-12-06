3a9871778ded07508f1cfb76dd2e79d7be357831
Akindele Ayorinde <AKINDELEAYORINDE55@GMAIL.COM>
2021-12-06 14:09:30 +0100

    add: initialCommit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+
+import React from "react";
+
+function Navbar() {
+    return (
+      <nav>
+        <img src='../images/airbnb-logo.png' alt='airbnb' className='nav--logo' />
+        </nav>
+    )
+  }
+
+  export default Navbar
+  
